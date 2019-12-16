8f6dcadc591c1034b68885848927bf445b09853a
Robert <48741931+bringmemyaxe@users.noreply.github.com>
2019-12-16 17:04:17 +0800

    Update signage.py

--- signage.py
+++ signage.py
@@ -4,11 +4,12 @@ import os
 
 r_list = list(string.digits + string.ascii_uppercase)
 full_r = [tup[0] + tup[1] for tup in itertools.product(r_list, r_list)]
+hider_path = 'dependencies/hider.X'
 while True:
     mode = input('''
 0 - add an idea
 1 - create a thing
-2 - create a thing connected to an existing one (without showing the list)
+2 - create a thing connected to an existing one
 3 - mark a thing as fully planned (hiding this thing from the list)
 4 - show unhidden things
 5 - show the list
@@ -22,36 +23,36 @@ choose mode: ''')
         mode0_idea = input('''add an idea:
 ''')
         for mode0_address in full_r:
-            mode0_path = 'ideas/ide' + mode0_address + '.J'
-            if os.path.exists(mode0_path):
+            path0 = 'ideas/ide' + mode0_address + '.J'
+            if os.path.exists(path0):
                 pass
             else:
                 try:
-                    with open(mode0_path, 'a', encoding='UTF-8') as mode0_adder:
-                        mode0_adder.write(mode0_idea)
+                    with open(path0, 'a', encoding='UTF-8') as adder:
+                        adder.write(mode0_idea)
                         print('saved at ' + 'ide' + mode0_address)
                         break
                 except FileNotFoundError:
                     os.makedirs('ideas/')
-                    with open(mode0_path, 'a', encoding='UTF-8') as mode0_adder:
-                        mode0_adder.write(mode0_idea)
+                    with open(path0, 'a', encoding='UTF-8') as adder:
+                        adder.write(mode0_idea)
                         print('saved at ' + 'ide' + mode0_address)
                         break
     elif mode == '1':
         mode1_temp = input('''create a core for dependencies:
 ''')
         for address in full_r:
-            mode1_path_Q = 'dependencies/Q/' + address + '.Q'
-            if os.path.exists(mode1_path_Q):
+            path1Q = 'dependencies/Q/' + address + '.Q'
+            if os.path.exists(path1Q):
                 pass
             else:
                 try:
-                    with open(mode1_path_Q, 'a', encoding='UTF-8') as mode1_description:
-                        mode1_description.write(mode1_temp)
+                    with open(path1Q, 'a', encoding='UTF-8') as descr:
+                        descr.write(mode1_temp)
                 except FileNotFoundError:
                     os.makedirs('dependencies/Q/')
-                    with open(mode1_path_Q, 'a', encoding='UTF-8') as mode1_description:
-                        mode1_description.write(mode1_temp)
+                    with open(path1Q, 'a', encoding='UTF-8') as descr:
+                        descr.write(mode1_temp)
                 mode1_path_X = 'dependencies/X/' + address + '.X'
                 try:
                     open(mode1_path_X, 'w', encoding='UTF-8')
@@ -62,22 +63,22 @@ choose mode: ''')
                 break
     elif mode == '2':
         mode2_link = input('''input an address for linking: ''')
-        mode2_linked_address = 'dependencies/X/' + mode2_link + '.X'
-        if os.path.exists(mode2_linked_address):
-            for mode2_new_link in full_r:
-                mode2_new_path_Q = 'dependencies/Q/' + mode2_new_link + '.Q'
-                if os.path.exists(mode2_new_path_Q):
+        path2X = 'dependencies/X/' + mode2_link + '.X'
+        if os.path.exists(path2X):
+            for link2 in full_r:
+                path2Q = 'dependencies/Q/' + link2 + '.Q'
+                if os.path.exists(path2Q):
                     pass
                 else:
-                    with open(mode2_linked_address, 'a', encoding='UTF-8') as mode2_linking:
-                        mode2_linking.write(mode2_new_link + '\n')
+                    with open(path2X, 'a', encoding='UTF-8') as linker:
+                        linker.write(link2 + '\n')
                     print('>', 'successfully linked', '<')
-                    print('>>', 'the address for a new thing is:', mode2_new_link, '<<')
+                    print('>> saved at:', link2, '<<')
                     mode2_temp = input('''create a dependency:
 ''')
-                    with open(mode2_new_path_Q, 'a', encoding='UTF-8') as mode2_new_description:
-                        mode2_new_description.write(mode2_temp)
-                    mode2_new_path_X = 'dependencies/X/' + mode2_new_link + '.X'
+                    with open(path2Q, 'a', encoding='UTF-8') as descr2:
+                        descr2.write(mode2_temp)
+                    mode2_new_path_X = 'dependencies/X/' + link2 + '.X'
                     open(mode2_new_path_X, 'w', encoding='UTF-8')
                     print('successfully saved')
                     break
@@ -87,18 +88,18 @@ choose mode: ''')
         mode3_address = input('''input an address for hiding: ''')
         mode3_path_check = 'dependencies/Q/' + mode3_address + '.Q'
         if os.path.exists(mode3_path_check):
-            with open('dependencies/hider.X', 'a', encoding='UTF-8') as hider:
+            with open(hider_path, 'a', encoding='UTF-8') as hider:
                 hider.write(mode3_address + '\n')
-            print('address', mode3_address, 'was successfully marked as hidden')
+            print('address', mode3_address, 'was marked as hidden')
         else:
             print('wrong address')
     elif mode == '4':
-        with open('dependencies/hider.X', encoding='UTF-8') as mode4_checker:
+        with open(hider_path, encoding='UTF-8') as mode4_checker:
             mode4_hidden_things = (mode4_checker.read().splitlines())
         for mode4_brute in full_r:
-            mode4_path = 'dependencies/Q/' + mode4_brute + '.Q'
+            path4 = 'dependencies/Q/' + mode4_brute + '.Q'
             try:
-                with open(mode4_path, encoding='UTF-8') as mode4_showing:
+                with open(path4, encoding='UTF-8') as mode4_showing:
                     if mode4_brute in mode4_hidden_things:
                         pass
                     else:
@@ -109,9 +110,9 @@ choose mode: ''')
                 pass
     elif mode == '5':
         for mode5_brute in full_r:
-            mode5_path = 'dependencies/Q/' + mode5_brute + '.Q'
+            path5 = 'dependencies/Q/' + mode5_brute + '.Q'
             try:
-                with open(mode5_path, encoding='UTF-8') as mode5_showing:
+                with open(path5, encoding='UTF-8') as mode5_showing:
                     print('')
                     print(mode5_brute + '.Q' + ':')
                     print(mode5_showing.read())
@@ -119,13 +120,13 @@ choose mode: ''')
                 pass
     elif mode == '6':
         mode6_first = input('''input an address for a thing: ''')
-        mode6_first_path = 'dependencies/X/' + mode6_first + '.X'
-        if os.path.exists(mode6_first_path):
+        path6 = 'dependencies/X/' + mode6_first + '.X'
+        if os.path.exists(path6):
             mode6_second = input('''input an address for the second thing that you need for the first one: ''')
             mode6_second_path = 'dependencies/Q/' + mode6_second + '.Q'
             if os.path.exists(mode6_second_path):
-                with open(mode6_first_path, 'a', encoding='UTF-8') as mode6_linking:
-                    mode6_linking.write(mode6_second + '\n')
+                with open(path6, 'a', encoding='UTF-8') as linker6:
+                    linker6.write(mode6_second + '\n')
                 print('successfully linked')
             else:
                 print('wrong address')
@@ -133,9 +134,9 @@ choose mode: ''')
             print('wrong address')
     elif mode == '7':
         for mode7_brute in full_r:
-            mode7_path = 'ideas/ide' + mode7_brute + '.J'
+            path7 = 'ideas/ide' + mode7_brute + '.J'
             try:
-                with open(mode7_path, encoding='UTF-8') as mode7_showing:
+                with open(path7, encoding='UTF-8') as mode7_showing:
                     print('')
                     print('ide' + mode7_brute + '.J' + ':')
                     print(mode7_showing.read())
@@ -186,7 +187,8 @@ choose mode: ''')
                     element_index = -1
 
 
-                    def recursio(element_index, path_index, file_index, adding_index, contents_index):
+                    def recursio(element_index, path_index, file_index,
+                                 adding_index, contents_index):
                         element_index += 1
                         path_index += 1
                         file_index += 1
@@ -213,7 +215,10 @@ choose mode: ''')
                                         if len(contents[contents_index]) == 0:
                                             contents_index -= 1
                                         else:
-                                            recursio(element_index, path_index, file_index, adding_index,
+                                            recursio(element_index,
+                                                     path_index,
+                                                     file_index,
+                                                     adding_index,
                                                      contents_index)
                                         current_chain.remove(element[element_index])
                                         break
@@ -226,7 +231,8 @@ choose mode: ''')
                         element_index -= 1
 
 
-                    recursio(element_index, path_index, file_index, adding_index, contents_index)
+                    recursio(element_index, path_index, file_index,
+                             adding_index, contents_index)
             except FileNotFoundError:
                 pass
         print(count)
