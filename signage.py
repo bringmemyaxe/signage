b908bb49cb534a0fe4751a30708ebe991a6a6182
Robert <48741931+bringmemyaxe@users.noreply.github.com>
2020-01-24 08:43:23 +0800

    Update signage.py

--- signage.py
+++ signage.py
@@ -1,247 +1,192 @@
+#                                  IDEAS
+# the list with ideas is just a list where you can add some ideas like
+# in a to-do list if you're afraid of forgetting something important
+#
+#                                  PIECES
+# the diagram contains pieces which might be connected like in causality:
+# the cause and consequence relationship; or like
+# the goal and problems relationship or anything else
+#
+#                                  ROOT-PIECE
+# a root-piece is a piece which isn't a dependency of other ones so far
+#
+#                                  DEPENDENCY-PIECE
+# a dependency-piece is a piece which is going to be added as already
+# connected to an existing one
 import string
 import itertools
 import os
 
-r_list = list(string.digits + string.ascii_uppercase)
-full_r = [tup[0] + tup[1] for tup in itertools.product(r_list, r_list)]
+digits_and_letters = list(string.digits + string.ascii_uppercase)
+digits_and_letters.remove('O')
+address_list = [tup[0] + tup[1] for tup in itertools.product(digits_and_letters, digits_and_letters)]
 hider_path = 'dependencies/hider.X'
 while True:
     mode = input('''
 0 - add an idea
-1 - create a thing
-2 - create a thing connected to an existing one
-3 - mark a thing as fully planned (hiding this thing from the list)
-4 - show unhidden things
-5 - show the list
-6 - connector
-7 - show the ideas
-8 - delete an idea
-9 - prioritise things
+1 - show the list of ideas
+2 - delete an idea
+3 - add a root-piece
+4 - add a dependency-piece
+5 - show the list of unhidden pieces
+6 - hide a piece from the list
+7 - connect two pieces
+8 - show the list of all pieces
+9 - prioritise pieces
 e - exit
 choose mode: ''')
     if mode == '0':
-        mode0_idea = input('''add an idea:
+        idea = input('''add an idea:
 ''')
-        for mode0_address in full_r:
-            path0 = 'ideas/ide' + mode0_address + '.J'
-            if os.path.exists(path0):
+        for address in address_list:
+            path = 'ideas/ide' + address + '.J'
+            if os.path.exists(path):
                 pass
             else:
                 try:
-                    with open(path0, 'a', encoding='UTF-8') as adder:
-                        adder.write(mode0_idea)
-                        print('saved at ' + 'ide' + mode0_address)
+                    with open(path, 'a', encoding='UTF-8') as new_file:
+                        new_file.write(idea)
+                        print('saved at ide' + address)
                         break
                 except FileNotFoundError:
                     os.makedirs('ideas/')
-                    with open(path0, 'a', encoding='UTF-8') as adder:
-                        adder.write(mode0_idea)
-                        print('saved at ' + 'ide' + mode0_address)
+                    with open(path, 'a', encoding='UTF-8') as new_file:
+                        new_file.write(idea)
+                        print('saved at ide' + address)
                         break
     elif mode == '1':
-        mode1_temp = input('''create a core for dependencies:
+        for address in address_list:
+            path = 'ideas/ide' + address + '.J'
+            try:
+                with open(path, encoding='UTF-8') as showing:
+                    print('')
+                    print('ide' + address + '.J:')
+                    print(showing.read())
+            except FileNotFoundError:
+                pass
+    elif mode == '2':
+        address = input('''input an address for deleting: ''')
+        path = 'ideas/ide' + address + '.J'
+        if os.path.exists(path):
+            os.remove(path)
+            print('successfully deleted')
+        else:
+            print('wrong address')
+    elif mode == '3':
+        piece = input('''add a root-piece:
 ''')
-        for address in full_r:
-            path1Q = 'dependencies/Q/' + address + '.Q'
-            if os.path.exists(path1Q):
+        for address in address_list:
+            path_for_Q = 'dependencies/Q/' + address + '.Q'
+            if os.path.exists(path_for_Q):
                 pass
             else:
                 try:
-                    with open(path1Q, 'a', encoding='UTF-8') as descr:
-                        descr.write(mode1_temp)
+                    with open(path_for_Q, 'w', encoding='UTF-8') as new_file:
+                        new_file.write(piece)
                 except FileNotFoundError:
                     os.makedirs('dependencies/Q/')
-                    with open(path1Q, 'a', encoding='UTF-8') as descr:
-                        descr.write(mode1_temp)
-                mode1_path_X = 'dependencies/X/' + address + '.X'
-                try:
-                    open(mode1_path_X, 'w', encoding='UTF-8')
-                except FileNotFoundError:
-                    os.makedirs('dependencies/X/')
-                    open(mode1_path_X, 'w', encoding='UTF-8')
-                print('>', 'successfully created at', address, '<')
-                break
-    elif mode == '2':
-        mode2_link = input('''input an address for linking: ''')
-        path2X = 'dependencies/X/' + mode2_link + '.X'
-        if os.path.exists(path2X):
-            for link2 in full_r:
-                path2Q = 'dependencies/Q/' + link2 + '.Q'
-                if os.path.exists(path2Q):
+                    with open(path_for_Q, 'w', encoding='UTF-8') as new_file:
+                        new_file.write(piece)
+                    path_for_X = 'dependencies/X/' + address + '.X'
+                    try:
+                        open(path_for_X, 'w', encoding='UTF-8')
+                    except FileNotFoundError:
+                        os.makedirs('dependencies/X/')
+                        open(path_for_X, 'w', encoding='UTF-8')
+                    print('> successfully created at ' + address + ' <')
+                    break
+    elif mode == '4':
+        root_address = input('''input an address for linking: ''')
+        root_path = 'dependencies/X/' + root_address + '.X'
+        if os.path.exists(root_path):
+            for dependency_address in address_list:
+                dependency_path = 'dependencies/Q/' + dependency_address + '.Q'
+                if os.path.exists(dependency_path):
                     pass
                 else:
-                    with open(path2X, 'a', encoding='UTF-8') as linker:
-                        linker.write(link2 + '\n')
-                    print('>', 'successfully linked', '<')
-                    print('>> saved at:', link2, '<<')
-                    mode2_temp = input('''create a dependency:
+                    with open(root_path, 'a', encoding='UTF-8') as connector:
+                        connector.write(dependency_address + '\n')
+                    print('successfully linked')
+                    print('>> saved at:', dependency_address, '<')
+                    piece = input('''add a dependency-piece:
 ''')
-                    with open(path2Q, 'a', encoding='UTF-8') as descr2:
-                        descr2.write(mode2_temp)
-                    mode2_new_path_X = 'dependencies/X/' + link2 + '.X'
-                    open(mode2_new_path_X, 'w', encoding='UTF-8')
+                    with open(dependency_path, 'a', encoding='UTF-8') as new_file:
+                        new_file.write(piece)
+                    dependency_path = 'dependencies/X/' + dependency_address + '.X'
+                    open(dependency_path, 'w', encoding='UTF-8')
                     print('successfully saved')
                     break
         else:
             print('wrong address')
-    elif mode == '3':
-        mode3_address = input('''input an address for hiding: ''')
-        mode3_path_check = 'dependencies/Q/' + mode3_address + '.Q'
-        if os.path.exists(mode3_path_check):
-            with open(hider_path, 'a', encoding='UTF-8') as hider:
-                hider.write(mode3_address + '\n')
-            print('address', mode3_address, 'was marked as hidden')
-        else:
-            print('wrong address')
-    elif mode == '4':
-        with open(hider_path, encoding='UTF-8') as mode4_checker:
-            mode4_hidden_things = (mode4_checker.read().splitlines())
-        for mode4_brute in full_r:
-            path4 = 'dependencies/Q/' + mode4_brute + '.Q'
+    elif mode == '5':
+        with open(hider_path, encoding='UTF-8') as hider_open:
+            hidden_pieces = hider_open.read().splitlines()
+        for address in address_list:
+            path = 'dependencies/Q/' + address + '.Q'
             try:
-                with open(path4, encoding='UTF-8') as mode4_showing:
-                    if mode4_brute in mode4_hidden_things:
+                with open(path, encoding='UTF-8') as showing:
+                    if address in hidden_pieces:
                         pass
                     else:
                         print('')
-                        print(mode4_brute + '.Q' + ':')
-                        print(mode4_showing.read())
-            except FileNotFoundError:
-                pass
-    elif mode == '5':
-        for mode5_brute in full_r:
-            path5 = 'dependencies/Q/' + mode5_brute + '.Q'
-            try:
-                with open(path5, encoding='UTF-8') as mode5_showing:
-                    print('')
-                    print(mode5_brute + '.Q' + ':')
-                    print(mode5_showing.read())
+                        print(address + '.Q:')
+                        print(showing.read())
             except FileNotFoundError:
                 pass
     elif mode == '6':
-        mode6_first = input('''input an address for a thing: ''')
-        path6 = 'dependencies/X/' + mode6_first + '.X'
-        if os.path.exists(path6):
-            mode6_second = input('''input an address for the second thing that you need for the first one: ''')
-            mode6_second_path = 'dependencies/Q/' + mode6_second + '.Q'
-            if os.path.exists(mode6_second_path):
-                with open(path6, 'a', encoding='UTF-8') as linker6:
-                    linker6.write(mode6_second + '\n')
+        address = input('''input an address for hiding: ''')
+        path = 'dependencies/Q/' + address + '.Q'
+        if os.path.exists(path):
+            with open(hider_path, 'a', encoding='UTF-8') as hider:
+                hider.write(address + '\n')
+            print('successfully hidden')
+        else:
+            print('wrong address')
+    elif mode == '7':
+        root_address = input('''input an address for a root-piece: ''')
+        root_path = 'dependencies/X/' + root_address + '.X'
+        if os.path.exists(root_path):
+            dependency_address = input('''input an address for a dependency-piece: ''')
+            dependency_path = 'dependencies/Q/' + dependency_address + '.Q'
+            if os.path.exists(dependency_path):
+                with open(root_path, 'a', encoding='UTF-8') as connector:
+                    connector.write(dependency_address + '\n')
                 print('successfully linked')
             else:
                 print('wrong address')
         else:
             print('wrong address')
-    elif mode == '7':
-        for mode7_brute in full_r:
-            path7 = 'ideas/ide' + mode7_brute + '.J'
-            try:
-                with open(path7, encoding='UTF-8') as mode7_showing:
-                    print('')
-                    print('ide' + mode7_brute + '.J' + ':')
-                    print(mode7_showing.read())
-            except FileNotFoundError:
-                pass
     elif mode == '8':
-        mode8_link = input('''input an address for deleting: ''')
-        mode8_path = 'ideas/ide' + mode8_link + '.J'
-        if os.path.exists(mode8_path):
-            os.remove(mode8_path)
-            print('successfully deleted')
-        else:
-            print('wrong address')
-    elif mode == '9':
-        path = []
-        file = []
-        contents = []
-        count = {}
-        adding = []
-        element = []
-        for i in range(1, 1001):
-            path.append(list())
-            file.append(list())
-            contents.append(list())
-            adding.append(list())
-            element.append(list())
-        for brute_force in full_r:
-            path_index = 0
-            path[path_index] = 'dependencies/X/' + brute_force + '.X'
-            file_index = 0
-            contents_index = 0
-            current_chain = []
+        for address in address_list:
+            path = 'dependencies/Q/' + address + '.Q'
             try:
-                with open(path[path_index], encoding='UTF-8') as file[file_index]:
-                    contents[contents_index] = (file[file_index].read().splitlines())
-                current_chain.append(brute_force)
-                print(current_chain)
-                adding_index = 0
-                if brute_force not in count:
-                    count[brute_force] = 1
-                else:
-                    adding[adding_index] = count.get(brute_force)
-                    adding[adding_index] += 1
-                    count[brute_force] = adding[adding_index]
-                if len(contents[contents_index]) == 0:
-                    pass
-                else:
-                    element_index = -1
-
-
-                    def recursio(element_index, path_index, file_index,
-                                 adding_index, contents_index):
-                        element_index += 1
-                        path_index += 1
-                        file_index += 1
-                        adding_index += 1
-                        for element[element_index] in contents[contents_index]:
-                            try:
-                                while 1:
-                                    path[path_index] = 'dependencies/X/' + element[element_index] + '.X'
-                                    contents_index += 1
-                                    with open(path[path_index], encoding='UTF-8') as file[file_index]:
-                                        contents[contents_index] = (file[file_index].read().splitlines())
-                                    if element[element_index] in current_chain:
-                                        contents_index -= 1
-                                        break
-                                    else:
-                                        current_chain.append(element[element_index])
-                                        print(current_chain)
-                                        if element[element_index] not in count:
-                                            count[element[element_index]] = 1
-                                        else:
-                                            adding[adding_index] = count.get(element[element_index])
-                                            adding[adding_index] += 1
-                                            count[element[element_index]] = adding[adding_index]
-                                        if len(contents[contents_index]) == 0:
-                                            contents_index -= 1
-                                        else:
-                                            recursio(element_index,
-                                                     path_index,
-                                                     file_index,
-                                                     adding_index,
-                                                     contents_index)
-                                        current_chain.remove(element[element_index])
-                                        break
-                            except FileNotFoundError:
-                                while 1:
-                                    print("EVERYTHING WENT WRONG")
-                        adding_index -= 1
-                        file_index -= 1
-                        path_index -= 1
-                        element_index -= 1
-
-
-                    recursio(element_index, path_index, file_index,
-                             adding_index, contents_index)
+                with open(path, encoding='UTF-8') as showing:
+                    print('')
+                    print(address + '.Q:')
+                    print(showing.read())
             except FileNotFoundError:
                 pass
-        print(count)
-        k = 1
-        for k in range(1, 100001):
-            for xx, yy in count.items():
-                if yy == k:
-                    print(xx, yy)
+    elif mode == '9':
+        counted_list = {}
+        value_to_add_to = []
+        # not finished
+        for address in address_list:
+            # not finished
+            try:
+                # not finished
+                value_to_add_to_depth = 0
+                if address not in counted_list:
+                    counted_list[address] = 1
+                else:
+                    value_to_add_to[value_to_add_to_depth] = counted_list.get(address)
+                    value_to_add_to[value_to_add_to_depth] += 1
+                    counted_list[address] = value_to_add_to[value_to_add_to_depth]
+                # not finished
+        # not finished
+        for address, priority in counted_list.items():
+            # not finished
+            print('the address', address, 'has priority', priority)
     elif mode == 'e':
         break
     else:
-        print('wrong input')
+        print('wrong mode')
